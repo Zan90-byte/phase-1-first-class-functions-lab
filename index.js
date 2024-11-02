424f61e78edc8efa1f254cbbe6344cc17cfb60be
Jean Raphael Barbosa Santos <jrbasan@hotmail.com>
2024-11-01 19:01:42 -0700

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+//console.log(returnFirstTwoDrivers(drivers));
+
+//*********************************** */
+
+//const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2);
+}
+
+//console.log(returnLastTwoDrivers(drivers));
+
+//************************************ */
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//*********************************** */
+
+function createFareMultiplier(number) {
+    return function(fare){
+        return number * fare;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+// console.log(fareDoubler(10));
+// console.log(fareTripler(10));
+
+//********************************** */
+
+
+function selectDifferentDrivers(drivers, cb) {
+    return cb(drivers);
+}
+
+// console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+// console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
